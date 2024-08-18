879eac19d4fc996de04cde3f5ff521dbfb512a62
Youssef Mahmoud <ymahmoud@sumerge.com>
2024-08-18 16:27:05 +0300

    Added one-to-many relationship between Course and Ratings

--- src/main/java/com/talent/dependency_injection/entities/Course.java
+++ src/main/java/com/talent/dependency_injection/entities/Course.java
@@ -1,5 +1,6 @@
 package com.talent.dependency_injection.entities;
 
+import java.util.List;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -7,6 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -34,8 +36,12 @@ public class Course implements Comparable<Course>{
     
     @Getter @Setter
     @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "assessmentId" )
+    @JoinColumn(name = "assessmentId")
     private Assessment assessment;
+
+    @Getter @Setter
+    @OneToMany(mappedBy = "course")
+    private List<Rating> ratings; 
 
     public Course (int id, String name, String description, int credit){
         this.id = id;
--- src/main/java/com/talent/dependency_injection/entities/Rating.java
+++ src/main/java/com/talent/dependency_injection/entities/Rating.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 
 public class Rating {
@@ -12,9 +14,10 @@ public class Rating {
     @Getter
     private int id;
     
-    @Column(nullable = false)
     @Getter
-    private int courseId;
+    @ManyToOne
+    @JoinColumn(name = "courseId")
+    private Course course;
     
     @Column(nullable = false)
     @Getter
