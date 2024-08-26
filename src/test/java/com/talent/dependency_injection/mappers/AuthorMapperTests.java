ab7d819874d4570ef79f837454a432e94dba3dbe
Youssef Mahmoud <ymahmoud@sumerge.com>
2024-08-26 12:05:47 +0300

    Removed redundant annotations

--- src/test/java/com/talent/dependency_injection/mappers/AuthorMapperTests.java
+++ src/test/java/com/talent/dependency_injection/mappers/AuthorMapperTests.java
@@ -5,17 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.junit.jupiter.MockitoSettings;
-import org.mockito.quality.Strictness;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.talent.dependency_injection.entities.Author;
 
-@ExtendWith(MockitoExtension.class)
-@MockitoSettings(strictness = Strictness.LENIENT)
 @SpringBootTest
 public class AuthorMapperTests {
     @Autowired
