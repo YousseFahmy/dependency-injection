a844b9b9e46808c1e980f6474fe2921d712664e9
Youssef Mahmoud <ymahmoud@sumerge.com>
2024-08-26 15:57:49 +0300

    Added method security on the Get All Courses endpoint

--- src/main/java/com/talent/dependency_injection/config/SecurityConfig.java
+++ src/main/java/com/talent/dependency_injection/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.talent.dependency_injection.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -15,6 +16,7 @@ import jakarta.servlet.DispatcherType;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
